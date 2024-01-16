97054bfafc504c99715abc9fccec0f32c54776d0
thawdezin <thawdezin@gmail.com>
2024-01-16 13:24:58 +0630

    increase rate limit 10 per hour, 14000 per day

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ CORS(app)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["1440 per day", "120 per hour"],
+    default_limits=["14000 per day", "600 per hour"],
     storage_uri="memory://",
 )
 app.wsgi_app = ProxyFix(app.wsgi_app)
